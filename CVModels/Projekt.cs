bb300808afda7418cde95b4f322cb970882a8e31
AugustDelin <august.del@live.se>
2023-12-18 14:41:32 +0100

    g

--- CVModels/Projekt.cs
+++ CVModels/Projekt.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CVModels
 {
-    internal class Projekt
+    public class Projekt
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        public string Beskrivning { get; set; }
+        public string AnvändarID { get; set; }
+
+        [ForeignKey(nameof(AnvändarID))]
+        public virtual User Användare { get; set; }
+
+
     }
 }
