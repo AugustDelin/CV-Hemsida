a08bc7408809f87d04bced6766885970214348df
AugustDelin <august.del@live.se>
2023-12-20 10:23:04 +0100

    Update ProjektController.cs

--- CV-Hemsida/Controllers/ProjektController.cs
+++ CV-Hemsida/Controllers/ProjektController.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CVDataLayer;
+using CVModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CV_Hemsida.Controllers
 {
     public class ProjektController : Controller
     {
+        private CVContext _dbContext;
         public IActionResult ProjectPage()
         {
-
+            List<Projekt> listAvProjekt = _dbContext.Projekts.ToList();
+            ViewBag.Meddelande = "Listan med produkter";
             // TODO: Implementera databaslogiken
             // var projekten = // Hämta projektdata från databasen med LINQ
 
-            return View(/*projekten*/); // Temporärt tills databaslogiken är implementerad
+            return View(listAvProjekt); // Temporärt tills databaslogiken är implementerad
 
         }
+
 
 
   
