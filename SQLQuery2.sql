930979bf13832146c71f104af7ff099d7347793e
vickrv221 <105204504+vickrv221@users.noreply.github.com>
2023-12-20 12:27:32 +0100

    SQL kod projekt

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO AspNetUsers (Id, Aktiv, EmailConfirmed, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount)
+VALUES 
+('user-id-1', 1, 0, 0, 0, 0, 0),
+('user-id-2', 1, 0, 0, 0, 0, 0),
+('user-id-3', 1, 0, 0, 0, 0, 0);
+
+INSERT INTO Projekt (Titel, Beskrivning, AnvändarId)
+VALUES 
+('Projekt 1', 'Podd-projektet', 'user-id-1'),
+('Projekt 2', 'Webbdesign-projektet', 'user-id-2'),
+('Projekt 3', 'UX-design-projektet', 'user-id-3');
+
+DELETE FROM Projekt WHERE Titel IN ('Projekt 1', 'Projekt 2', 'Projekt 3');
+
