f1a60ed9f17591a0ba379bc9c9625e91612b699b
AugustDelin <august.del@live.se>
2023-12-20 12:26:26 +0100

    profilpage

---
+++ CV-Hemsida/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using CVDataLayer;
+using CVModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV_Hemsida.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private CVContext _dbContext;
+
+        public ProfileController(CVContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult ProfilePage()
+        {
+            List<Användare> listAvProfiler = _dbContext.Users.ToList();
+            ViewBag.Meddelande = "Listan med profiler";
+            // TODO: Implementera databaslogiken
+            // var projekten = // Hämta projektdata från databasen med LINQ
+
+            return View(listAvProfiler); // Temporärt tills databaslogiken är implementerad
+
+        }
+
+
+
+
+    }
+
+
+
+}
