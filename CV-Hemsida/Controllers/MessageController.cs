060e35cb7b97d1e4bc7a220f6241747da92d9a3b
erikbopettersson <104985980+erikbopettersson@users.noreply.github.com>
2023-12-28 19:47:55 +0100

    meddelande
    meddelande

---
+++ CV-Hemsida/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CV_Hemsida.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Message()
+        {
+            return View();
+        }
+    }
+}
---
+++ CVModels/ViewModels/SendMessageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVModels.ViewModels
+{
+    public class SendMessageViewModel
+    {
+        [Required(ErrorMessage = "Du måste ha innehåll.")]
+        public virtual string Innehåll { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange ett namn.")]
+        public virtual string Avsändare { get; set; }
+        public virtual string Mottagare { get; set; }
+
+
+
+    }
+}
+
