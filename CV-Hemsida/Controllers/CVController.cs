9c3b2909486b57ef3837fd9f0b25477935e08540
AugustDelin <august.del@live.se>
2024-01-02 14:23:36 +0100

    cv fix

--- CV-Hemsida/Controllers/CVController.cs
+++ CV-Hemsida/Controllers/CVController.cs
@@ -156,6 +156,85 @@ namespace CV_Hemsida.Controllers
             }
         }
 
+        public IActionResult ChangeCV(int id)
+        {
+            var cvToChange = _dbContext.CVs.Find(id);
+
+            if (cvToChange == null)
+            {
+                return NotFound(); // If CV is not found, return NotFound
+            }
+
+            var changeCVViewModel = new ChangeCVViewModel
+            {
+                Kompetenser = cvToChange.Kompetenser,
+                Utbildningar = cvToChange.Utbildningar,
+                TidigareErfarenhet = cvToChange.TidigareErfarenhet,
+                ProfilbildPath = cvToChange.ProfilbildPath
+            };
+
+            return View(changeCVViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ChangeCV(int id, ChangeCVViewModel model, IFormFile ProfilbildPath)
+        {
+            // Remove the ModelState error for ProfilbildPath
+            ModelState.Remove("ProfilbildPath");
+
+            if (ModelState.IsValid)
+            {
+                var cvToChange = _dbContext.CVs.Find(id);
+
+                if (cvToChange == null)
+                {
+                    return NotFound(); // If CV is not found, return NotFound
+                }
+
+                // Remove the existing profile picture file
+                if (!string.IsNullOrEmpty(cvToChange.ProfilbildPath))
+                {
+                    var existingFilePath = Path.Combine("wwwroot/Bilder", cvToChange.ProfilbildPath);
+
+                    if (System.IO.File.Exists(existingFilePath))
+                    {
+                        System.IO.File.Delete(existingFilePath);
+                    }
+                }
+
+                // Generate a unique file name or use the user's ID as the file name
+                var fileName = cvToChange.AnvändarId + "_" + Guid.NewGuid().ToString() + "_" + Path.GetFileName(ProfilbildPath.FileName);
+
+                // Specify the path where the file will be saved
+                var filePath = Path.Combine("wwwroot/Bilder", fileName);
+
+                // Save the file to the server
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    ProfilbildPath.CopyTo(fileStream);
+                }
+
+                // Set the ProfilbildPath property to the new file path
+                cvToChange.ProfilbildPath = fileName;
+
+                // Update other CV properties with the new values
+                cvToChange.Kompetenser = model.Kompetenser;
+                cvToChange.Utbildningar = model.Utbildningar;
+                cvToChange.TidigareErfarenhet = model.TidigareErfarenhet;
+
+                // Update the CV in the database
+                _dbContext.CVs.Update(cvToChange);
+                _dbContext.SaveChanges();
+
+                // Redirect to the CV page
+                return RedirectToAction("CVPage");
+            }
+
+            // If the model is not valid,
+            // return to the ChangeCV view with errors
+            return View(model);
+        }
+
 
         public ActionResult ResourceNotFound()
         {
