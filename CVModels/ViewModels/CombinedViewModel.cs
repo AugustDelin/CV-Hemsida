586220475c4fbb6c1a6ac8ddcffbeb2f8b112be3
vickrv221 <105204504+vickrv221@users.noreply.github.com>
2024-01-03 19:54:16 +0100

    Viewmodels

---
+++ CVModels/ViewModels/CombinedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVModels.ViewModels
+{
+    public class CombinedViewModel
+    {
+        public IEnumerable<CVViewModel> CVs { get; set; }
+        public StartPageViewModel StartPage { get; set; }
+    }
+}
