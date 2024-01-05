dbf45f3e31a52cfd7e4bb2d63fa1fbc893dca32b
Buggish11 <105172988+Buggish11@users.noreply.github.com>
2024-01-05 21:06:07 +0100

    kommentarer AccountController.cs

--- CV-Hemsida/Controllers/AccountController.cs
+++ CV-Hemsida/Controllers/AccountController.cs
@@ -1,44 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using CVModels;
-using CVDataLayer;
-using Microsoft.AspNetCore.Identity;
-using CVModels.ViewModel;
-using Microsoft.AspNetCore.Authorization;
-using CVModels.ViewModels;
-using Microsoft.EntityFrameworkCore;
-using System;
-using CV_SITE.Repositories;
+using CVModels; // Importerar modellerna för användare och person
+using CVDataLayer; // Importerar dataåtkomstlagret
+using Microsoft.AspNetCore.Identity; // Importerar Identity-funktioner för hantering av användare och inloggning
+using CVModels.ViewModel; // Importerar vymodeller för användare och person
+using Microsoft.AspNetCore.Authorization; // Importerar autorisationsattribut
+using CVModels.ViewModels; // Importerar vymodeller för användare och person
+using Microsoft.EntityFrameworkCore; // Importerar Entity Framework Core för databashantering
+using System; // Importerar systembiblioteket för allmänna funktioner
+using CV_SITE.Repositories; // Importerar specifika repositories
 
 namespace CV_Hemsida.Controllers
 {
     public class AccountController : BaseController
     {
-        private readonly UserManager<Användare>? _userManager;
-        private readonly SignInManager<Användare>? _signInManager;
-        private readonly CVContext _dbContext;
+        private readonly UserManager<Användare>? _userManager; // Hanterar användaråtgärder
+        private readonly SignInManager<Användare>? _signInManager; // Hanterar inloggning/utloggning
+        private readonly CVContext _dbContext; // Databaskontext för användar- och personinformation
 
+        // Konstruktor för AccountController
         public AccountController(UserManager<Användare> userManager, SignInManager<Användare> signInManager, CVContext dbContext) : base(dbContext)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _dbContext = dbContext;
-            
+            _userManager = userManager; // Initierar UserManager
+            _signInManager = signInManager; // Initierar SignInManager
+            _dbContext = dbContext; // Initierar CVContext
+
         }
 
-        
 
+        // Metod för att visa inloggningsvyn
         public IActionResult Login()
         {
-            SetMessageCount();
-            return View();
+            SetMessageCount(); // Uppdaterar meddelandelicensräknaren
+            return View(); // Visar inloggningsvyn
         }
 
 
+        // Metod för att hantera inloggning
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             SetMessageCount();
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) // Uppdaterar meddelandelicensräknaren
             {
                 var user = await _userManager.FindByNameAsync(model.UserName);
 
@@ -61,29 +63,32 @@ namespace CV_Hemsida.Controllers
         }
 
 
-        
+        // Metod för att logga ut användaren
         public async Task<IActionResult> Logout()
         {
-            SetMessageCount();
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Login");
+            SetMessageCount(); // Uppdaterar meddelandelicensräknaren
+            await _signInManager.SignOutAsync(); // Loggar ut användaren
+            return RedirectToAction("Login"); // Omdirigerar till inloggningssidan
         }
 
 
 
-
+        // Metod för att visa inloggad-vyn
         public IActionResult Inloggad()
         {
-            return View();
+            return View(); // Visar inloggad-vyn
         }
 
+
+        // Metod för att visa registreringsvyn för användare
         public IActionResult RegisterUser()
         {
-            SetMessageCount();
-            return View();
+            SetMessageCount(); // Uppdaterar meddelandelicensräknaren
+            return View(); // Visar registreringsvyn för användare
         }
 
 
+        // Metod för att hantera registrering av användare
         [HttpPost]
         public async Task<IActionResult> RegisterUser(RegisterUserViewModel model)
         {
@@ -113,13 +118,15 @@ namespace CV_Hemsida.Controllers
             return View(model);
         }
 
+        // Metod för att visa registreringsvyn för person
         public IActionResult RegisterPerson()
         {
-            SetMessageCount();
-            return View();
+            SetMessageCount(); // Uppdaterar meddelandelicensräknaren
+            return View(); // Visar registreringsvyn för person
         }
 
 
+        // Metod för att hantera registrering av person
         [HttpPost]
         public async Task<IActionResult> RegisterPerson(RegisterPersonViewModel model)
         {
@@ -157,7 +164,7 @@ namespace CV_Hemsida.Controllers
 
 
 
-
+        // Metod för att ändra lösenord
         [HttpPost]
         public async Task<IActionResult> EditPassword(EditPasswordViewModel model)
         {
@@ -199,14 +206,16 @@ namespace CV_Hemsida.Controllers
             }
         }
 
+        // Metod för att visa bekräftelsesidan för lösenordsändring
         public IActionResult PasswordConfirmation()
         {
-            return View();
+            return View(); // Visar bekräftelsesidan för lösenordsändring
         }
 
+        // Metod för att visa vyn för lösenordsändring
         public IActionResult EditPassword()
         {
-            return View();
+            return View(); // Visar vyn för lösenordsändring
         }
     }
 }
