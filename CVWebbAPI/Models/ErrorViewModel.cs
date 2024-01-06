627f495f6dbeb27e5c476eaf8c64ad646433619c
Buggish11 <105172988+Buggish11@users.noreply.github.com>
2024-01-06 02:02:33 +0100

    kommentarer ErrorViewModel.cs

--- CVWebbAPI/Models/ErrorViewModel.cs
+++ CVWebbAPI/Models/ErrorViewModel.cs
@@ -1,3 +1,7 @@
+//Denna kod definierar en enkel modell, ErrorViewModel,
+//som innehåller en egenskap RequestId för att lagra en begärande identifierare och
+//en metod ShowRequestId för att kontrollera om RequestId är giltig och ska visas.
+
 namespace CVWebbAPI.Models
 {
 	public class ErrorViewModel
